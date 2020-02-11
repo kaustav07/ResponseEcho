6d695a5c82b1f4da4abb2d9feb835719ee9d4dd7
Kaustav Chatterjee <kaustav.chatterjee@nearbuy.com>
2020-02-11 17:35:13 +0530

    bug fixes

--- src/main/java/com/github/simonpercic/responseecho/Utils.kt
+++ src/main/java/com/github/simonpercic/responseecho/Utils.kt
@@ -1,10 +1,13 @@
 package com.github.simonpercic.responseecho
 
 import com.github.simonpercic.oklog.shared.data.HeaderData
+import com.google.gson.Gson
+import com.google.gson.JsonObject
 import org.omg.CORBA.NameValuePair
 import java.net.URI
 import java.net.URL
 import java.net.URLDecoder
+import java.net.URLEncoder
 
 
 fun List<HeaderData>.authorization() : String? = this.associate { Pair(it.name,it.value) }["Authorization"]
@@ -12,10 +15,18 @@ fun List<HeaderData>.authorization() : String? = this.associate { Pair(it.name,i
 fun String?.getUrlQueryParam() : List<HeaderData> {
     val queryList = ArrayList<HeaderData>()
     this?.let {
-        val url = URL(it).query
+        val url = URL(URLDecoder.decode(it)).query
        url.split("&").forEach {
-           val (name,value) = it.split("=")
-           queryList.add(HeaderData(name, URLDecoder.decode(value)))
+           if(it.startsWith('{').not()){
+               val (name,value) = it.split("=")
+               queryList.add(HeaderData(name, URLDecoder.decode(value)))
+           }else{
+               val obj = Gson().fromJson<JsonObject>(it,JsonObject::class.java)
+               obj.entrySet().forEach {
+                   queryList.add(HeaderData(it.key, it.value.toString()))
+               }
+           }
+
        }
     }
 
